ab8e99a0852da8194f07fc4eb56875d3b5109401
AsherW13 <aweitz@zagmail.gonzaga.edu>
2025-01-07 21:03:00 -0800

    Implemented API's for booking and menu tables

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from . import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'booking', views.BookingViewSet)
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('items/', views.MenuItemView.as_view(), name='menu-list-create'),
+    path('items/<int:pk>/', views.SingleMenuItemView.as_view(), name='menu-detail-update-delete'),
 ]
