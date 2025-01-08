974ff471c7591e0c5f1ef2516f5943506d95d6f3
AsherW13 <aweitz@zagmail.gonzaga.edu>
2025-01-07 20:21:53 -0800

    Added models

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Menu(models.Model):
+    id = models.AutoField(primary_key=True)  # ID with auto-increment
+    title = models.CharField(max_length=255)  # Title with max length of 255
+    price = models.DecimalField(max_digits=10, decimal_places=2)  # Price with two decimal places
+    inventory = models.IntegerField()  # Inventory as an integer
+
+    def __str__(self):
+        return self.title  # String representation
+
+class Booking(models.Model):
+    id = models.AutoField(primary_key=True)  # ID with auto-increment
+    name = models.CharField(max_length=255)  # Name with max length of 255
+    no_of_guests = models.IntegerField()  # Number of guests as an integer
+    booking_date = models.DateTimeField()  # Booking date as a datetime field
+
+    def __str__(self):
+        return f"{self.name} - {self.booking_date}"
