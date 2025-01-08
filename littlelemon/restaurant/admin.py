a2c1a5f3ccb62b841a2534b2e6f26a73012d9192
AsherW13 <aweitz@zagmail.gonzaga.edu>
2025-01-07 22:04:59 -0800

    Implemented token based authentication for the booking api

--- littlelemon/restaurant/admin.py
+++ littlelemon/restaurant/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Menu, Booking
+from rest_framework.authtoken.models import Token
 
 admin.site.register(Menu)
 admin.site.register(Booking)
+admin.site.register(Token)
--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter()
 router.register(r'booking', views.BookingViewSet)
@@ -9,4 +10,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('items/', views.MenuItemView.as_view(), name='menu-list-create'),
     path('items/<int:pk>/', views.SingleMenuItemView.as_view(), name='menu-detail-update-delete'),
+    path('api-token-auth/', obtain_auth_token),
 ]
